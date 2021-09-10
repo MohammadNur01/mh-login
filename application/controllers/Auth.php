0961c062e0c202190930255c5c62c2471f757c76
MohammadNur01 <mohammadnurrossifumi@gmail.com>
2021-09-10 15:24:58 +0700

    edit auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -134,6 +134,9 @@ class Auth extends CI_Controller
     if ($type == 'verify') {
       $this->email->subject('Account Verification');
       $this->email->message('Click this link to verify your account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '& token=' . urlencode($token) . '">Activated</a>');
+    } else if ($type == 'forgot') {
+      $this->email->subject('Reset Password!');
+      $this->email->message('Click this link to verify your account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '& token=' . urlencode($token) . '">Activated</a>');
     }
 
     if ($this->email->send()) {
@@ -197,9 +200,33 @@ class Auth extends CI_Controller
 
   public function forgotPassword()
   {
-    $data['title'] = 'Forgot Password';
-    $this->load->view('templates/auth_header', $data);
-    $this->load->view('auth/forgot-password');
-    $this->load->view('templates/auth_footer');
+    $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+    if ($this->form_validation->run() == false) {
+      $data['title'] = 'Forgot Password';
+      $this->load->view('templates/auth_header', $data);
+      $this->load->view('auth/forgot-password');
+      $this->load->view('templates/auth_footer');
+    } else {
+      $email = $this->input->post('email');
+      $user = $this->db->get_where('user', ['email' => $email, 'is_active' => 1])->row_array();
+
+      if ($user) {
+        $token = base64_encode(random_bytes(32));
+        $user_token = [
+          'email' => $email,
+          'token' => $token,
+          'date_created' => time()
+        ];
+
+        $this->db->insert('user_token', $user_token);
+        $this->_sendEmail($token, 'forgot');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Please check your email to reset your password!</div>');
+        redirect('auth/forgotpassword');
+      } else {
+        $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered or activated!</div>');
+        redirect('auth/forgotpassword');
+      }
+    }
   }
 }
